36cbe20cb2cd9ca539c47d62119191508bd5d624
alexvangogen <alexvangogen@gmail.com>
2018-08-31 04:19:05 +0300

    Minor: replace @IgnoreParameter annotation to less interesting parameter

--- app/src/main/kotlin/summer/practice/kapt/classes/Class7.kt
+++ app/src/main/kotlin/summer/practice/kapt/classes/Class7.kt
@@ -5,6 +5,6 @@ import summer.practice.kapt.IgnoreParameter
 
 class Class7<T> {
     fun <U: T, V: U> someFun(u: List<U>, v: MutableMap<out V, Map<T, List<*>>>) = if (u.isNotEmpty()) u.first() else null
-    @DumpFunction fun <U: T, V: U> someFun(u: U, @IgnoreParameter v: MutableMap<out V, Map<T, List<*>>>) = u
+    @DumpFunction fun <U: T, V: U> someFun(@IgnoreParameter u: U, v: MutableMap<out V, Map<T, List<*>>>) = u
     @DumpFunction inline fun <reified T, S: T> Map<T, S>.nextFun() {}
 }
